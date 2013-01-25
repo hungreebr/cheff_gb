26c2b611c2825b9cacf4514c7110734f52e9afdc
hungreebr <marketing@hungree.com.br>
2013-01-25 14:13:59 -0300

    firt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheffGb::Application.config.secret_token = 'd8f25d286342a5810430abd9a46610945f6e3afedfe8e0c21d874b0c7b04f0e02d1fea235387addffd7bd86193b58574b98a9672ae5d18a659d5d876666f18bc'
